8f67558c69db94a9e0b788a561403114d19fc7ce
uandhgroup <uday@uandh.in>
2024-04-23 16:33:34 +0530

    done
    done

--- src/components/StarOnGithub.svelte
+++ src/components/StarOnGithub.svelte
@@ -6,5 +6,5 @@
 	href="https://techtips.hhf.co.in/"
 	class="flex-shrink-0 flex items-center gap-2 bg-gray-100 dark:bg-gray-800 px-4 h-10 rounded-full hover:bg-yellow-400 dark:hover:bg-yellow-700"
 >
-	Be our reader <GithubLogo />
+	Read <GithubLogo />
 </a>
